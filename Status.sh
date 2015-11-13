8136d2c5d90ae94898fdd0756d0d57efe0108a4b
Paul Galow <paul.galow@gmail.com>
2015-11-13 15:41:42 +0100

    Bluetooth and Airport standards added

--- Status.sh
+++ Status.sh
@@ -10,13 +10,43 @@ printf "Displaying current status overview...\n\n"
 serial=$(system_profiler SPHardwareDataType | grep "Serial Number" | awk '{ print $4 }')
 currentuser=$(id -F)
 hostname=$(scutil --get HostName)
-ssid=$(/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport -I | awk '/[^B]SSID/ { print $2 }')
-printf "Serial number:\t%s\nCurrent user:\t%s\nHost name:\t%s\nCurrent WiFi:\t%s\n" "$serial" "$currentuser" "$hostname" "$ssid"
+printf "Serial number:\t%s\nCurrent user:\t%s\nHost name:\t%s\n" "$serial" "$currentuser" "$hostname"
 
-# Display MAC addresses of primary and secondary networking interfaces
+# Display Bluetooth version
+# Reference: https://www.bluetooth.org/en-us/specification/assigned-numbers/link-manager
+lmp=$(system_profiler -detailLevel full SPBluetoothDataType | grep "LMP Version" | awk '{ print $3 }' | cut -c 3)
+if [[ $lmp -eq 8 ]]; then
+	bluetooth="4.2"
+elif [[ $lmp -eq 7 ]]; then
+	bluetooth="4.1"
+elif [[ $lmp -eq 6 ]]; then
+	bluetooth="4.0"
+elif [[ $lmp -eq 5 ]]; then
+	bluetooth="3.0 + HS"
+elif [[ $lmp -eq 4 ]]; then
+	bluetooth="2.1 + EDR"
+elif [[ $lmp -eq 3 ]]; then
+	bluetooth="2.0 + EDR"
+elif [[ $lmp -eq 2 ]]; then
+	bluetooth="1.2"
+elif [[ $lmp -eq 1 ]]; then
+	bluetooth="1.1"
+elif [[ $lmp -eq 0 ]]; then
+	bluetooth="1.0b"
+elif [[ $lmp -gt 8 ]]; then
+	printf "\nBluetooth:\t4.2+\n"
+fi
+printf "Bluetooth:\t%s\n" "$bluetooth"
+
+# Display supported WiFi standards
+airportstandards=$(system_profiler -detailLevel mini SPAirPortDataType | grep "Supported PHY Modes" | awk '{ print $4" "$5 }')
+printf "AirPort:\t%s\n" "$airportstandards"
+
+# Display MAC addresses of primary and secondary networking interfaces and current WiFi
+ssid=$(/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport -I | awk '/[^B]SSID/ { print $2 }')
 primary=$(ifconfig en0 | grep -o -E '([[:xdigit:]]{1,2}:){5}[[:xdigit:]]{1,2}')
 secondary=$(ifconfig en1 | grep -o -E '([[:xdigit:]]{1,2}:){5}[[:xdigit:]]{1,2}')
-printf "\nMAC en0:\t%s\nMAC en1:\t%s\n" "$primary" "$secondary"
+printf "\nCurrent WiFi:\t%s\nMAC en0:\t%s\nMAC en1:\t%s\n" "$ssid" "$primary" "$secondary"
 
 # Check OS version
 os=$(sw_vers -productVersion)
