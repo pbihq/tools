cc42ab30fbd545695134f4c1e460a7704414d241
Paul Galow <paul.galow@gmail.com>
2015-11-13 12:31:56 +0100

    Office software versions added

--- Status.sh
+++ Status.sh
@@ -13,6 +13,11 @@ hostname=$(scutil --get HostName)
 ssid=$(/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport -I | awk '/[^B]SSID/ { print $2 }')
 printf "Serial number:\t%s\nCurrent user:\t%s\nHost name:\t%s\nCurrent WiFi:\t%s\n" "$serial" "$currentuser" "$hostname" "$ssid"
 
+# Display MAC addresses of primary and secondary networking interfaces
+primary=$(ifconfig en0 | grep -o -E '([[:xdigit:]]{1,2}:){5}[[:xdigit:]]{1,2}')
+secondary=$(ifconfig en1 | grep -o -E '([[:xdigit:]]{1,2}:){5}[[:xdigit:]]{1,2}')
+printf "\nMAC en0:\t%s\nMAC en1:\t%s\n" "$primary" "$secondary"
+
 # Check OS version
 os=$(sw_vers -productVersion)
 printf "\nOS X:\t%s\n" "$os"
@@ -22,10 +27,15 @@ runningtime=$(uptime | grep -ohe 'up .*' | sed 's/,//g' | awk '{ print $2" "$3 }
 load=$(uptime | grep -ohe 'load average[s:][: ].*' | awk '{ print $3 }')
 printf "Uptime:\t%s\nLoad:\t%s\n" "$runningtime" "$load"
 
+# Check for Filevault and OS X firewall
+filevault=$(fdesetup status)
+firewall=$(/usr/libexec/ApplicationFirewall/socketfilterfw --getglobalstate | awk '{ print $1" "$2" "$3 }')
+printf "\n%s\n%s\n" "$filevault" "$firewall"
+
 # Check free disk space
 diskspace=$(df -Hl | grep "disk1" | awk '{ print $4 }')
 diskusage=$(df -Hl | grep "disk1" | awk '{ print $5 }')
-printf "\nFree disk space left:\t%s\nDisk usage in percent:\t%s\n" "$diskspace" "$diskusage"
+printf "\nHDD usage:\t%s | %s left\n" "$diskusage" "$diskspace"
 
 # Calculate Apple Mail database size
 if [[ $os = 10.11.* ]]; then
@@ -34,17 +44,13 @@ else
 	mailversion="V2"
 fi
 maildbsize=$(ls -lnah ~/Library/Mail/$mailversion/MailData | grep -E 'Envelope Index$' | awk '{ print $5 }')B
-printf "\nMail database size:\t%s\n" "$maildbsize"
+printf "\nMail db size:\t%s\n" "$maildbsize"
 
-# Check for Filevault and OS X firewall
-filevault=$(fdesetup status)
-firewall=$(/usr/libexec/ApplicationFirewall/socketfilterfw --getglobalstate | awk '{ print $1" "$2" "$3 }')
-printf "\n%s\n%s\n" "$filevault" "$firewall"
-
-# Display MAC addresses of primary and secondary networking interfaces
-primary=$(ifconfig en0 | grep -o -E '([[:xdigit:]]{1,2}:){5}[[:xdigit:]]{1,2}')
-secondary=$(ifconfig en1 | grep -o -E '([[:xdigit:]]{1,2}:){5}[[:xdigit:]]{1,2}')
-printf "\nMAC address en0:\t%s\nMAC address en1:\t%s\n" "$primary" "$secondary"
+# Check for Office version installed
+pptversion=$(defaults read /Applications/Microsoft\ Office\ 2011/Microsoft\ PowerPoint.app/Contents/version.plist CFBundleVersion)
+excelversion=$(defaults read /Applications/Microsoft\ Office\ 2011/Microsoft\ Word.app/Contents/version.plist CFBundleVersion)
+wordversion=$(defaults read /Applications/Microsoft\ Office\ 2011/Microsoft\ Excel.app/Contents/version.plist CFBundleVersion)
+printf "\nPPT:\tv%s\nWord:\tv%s\nExcel:\tv%s\n" "$pptversion" "$excelversion" "$wordversion"
 
 # Check for OS X software updates
 echo
