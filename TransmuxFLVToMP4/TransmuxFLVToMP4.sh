c00f4b8829d1321f6ba3c9d1b0b68fad6d4f0197
Paul Galow <paul.galow@gmail.com>
2018-07-27 10:44:59 +0200

    Minor improvements to increase resilience and fault-tolerance

--- TransmuxFLVToMP4/TransmuxFLVToMP4.sh
+++ TransmuxFLVToMP4/TransmuxFLVToMP4.sh
@@ -4,16 +4,20 @@
 
 # #################################################################################################
 # Set up logging
-info()    { echo "[INFO] $*" 1> >(sed $'s,.*,\e[32m&\e[m,'); }
-warning() { echo "[WARNING] $*" 1> >(sed $'s,.*,\e[33m&\e[m,'); }
-error()   { echo "[ERROR] $*" 1> >(sed $'s,.*,\e[35m&\e[m,'); }
-fatal()   { echo "[FATAL] $*" 1> >(sed $'s,.*,\e[31m&\e[m,'); exit 1; }
+info()    { echo "[INFO] $*" 1> >(sed $'s,.*,\e[32m&\e[m,'); awk ""; }
+warning() { echo "[WARNING] $*" 1> >(sed $'s,.*,\e[33m&\e[m,'); awk ""; }
+error()   { echo "[ERROR] $*" 1> >(sed $'s,.*,\e[35m&\e[m,'); awk ""; }
+fatal()   { echo "[FATAL] $*" 1> >(sed $'s,.*,\e[31m&\e[m,'); awk ""; exit 1; }
 
 # #################################################################################################
 # Set unofficial Bash strict mode
 # Source: https://dev.to/thiht/shell-scripts-matter
 set -euo pipefail
 IFS=$'\n\t'
+
+# Set working directory so that script can be run independent of location
+DIR="${BASH_SOURCE%/*}"
+if [[ ! -d "$DIR" ]]; then DIR="$PWD"; fi
 
 # Enable extended globbing. Expand file name patterns which match no files to a null string.
 shopt -s extglob nullglob
@@ -27,17 +31,17 @@ function checkOS() {
 
 # Check if FFmpeg is installed
 function checkFFmpegInstalled() {
-	which -s ffmpeg
+	command -v ffmpeg > /dev/null
 }
 
 # Transmuxes FLV files to MP4
 function transmuxFLVToMP4 {
-  for filename in *.+(flv|FLV); do
+  for filename in "$DIR"/*.+(flv|FLV); do
     # Extract filename without file extension
     local filenameBase=${filename%%.*}
     # Display info to user
     info "*** Transmuxing '$filename'... ***"
-    "$(which ffmpeg)" \
+    "$(command -v ffmpeg)" \
       -i "$filename" \
       -codec copy \
       "$filenameBase.mp4" \
@@ -47,6 +51,7 @@ function transmuxFLVToMP4 {
 }
 
 # Run functions
+trap exit 0 SIGINT # Press Ctrl+C to exit
 checkOS || { fatal "It seems you are not running macOS. Exiting..."; }
 checkFFmpegInstalled || { fatal "FFmpeg not found. Please install FFmpeg on your machine. See: https://trac.ffmpeg.org/wiki/CompilationGuide/MacOSX#ffmpegthroughHomebrew"; }
 transmuxFLVToMP4 || { fatal "Error transmuxing '$filename' to MP4"; }
