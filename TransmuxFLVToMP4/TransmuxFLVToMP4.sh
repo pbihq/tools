cd12bb1fd675f23420c87378f12ddc9a1cdd8306
Paul Galow <paul.galow@gmail.com>
2018-12-12 18:24:01 +0100

    Improve error handling. Add feature to provide target folder as a parameter

--- TransmuxFLVToMP4/TransmuxFLVToMP4.sh
+++ TransmuxFLVToMP4/TransmuxFLVToMP4.sh
@@ -2,13 +2,14 @@
 
 # Transmux FLV files to MP4 without re-encoding.
 
+# ##############################################################################
+# Set global properties
+readonly targetFolder="$1"
 # #################################################################################################
 # Set up logging
 info()    { echo "[INFO] $*" 1> >(sed $'s,.*,\e[32m&\e[m,'); awk ""; }
 warning() { echo "[WARNING] $*" 1> >(sed $'s,.*,\e[33m&\e[m,'); awk ""; }
-error()   { echo "[ERROR] $*" 1> >(sed $'s,.*,\e[35m&\e[m,'); awk ""; }
-fatal()   { echo "[FATAL] $*" 1> >(sed $'s,.*,\e[31m&\e[m,'); awk ""; exit 1; }
-
+error()   { echo "[ERROR] $*" 1> >(sed $'s,.*,\e[35m&\e[m,'); awk ""; exit 1; }
 # #################################################################################################
 # Set unofficial Bash strict mode
 # Source: https://dev.to/thiht/shell-scripts-matter
@@ -21,26 +22,47 @@ shopt -s extglob nullglob
 # #################################################################################################
 
 # Check if operating system is macOS
-function checkOS() {
+checkOS() {
   if [[ ! "$(uname)" = "Darwin" ]]; then return 1; fi
 }
 
-# Check if FFmpeg is installed
-function checkFFmpegInstalled() {
-	command -v ffmpeg > /dev/null
+# Check if program is installed
+checkInstalled() {
+  command -v "$1" > /dev/null
+}
+
+# Check for FLV files in target folder
+checkFiles() {
+  local counter=0
+
+  local -r outputFolder="${1:-"."}"
+  cd "$outputFolder"
+
+  for filename in *.+(flv|FLV); do
+    if [[ -f "$filename" ]]; then
+      (( counter++ ))
+    fi
+  done
+
+  if [ $counter == 0 ]; then
+    return 1
+  fi
 }
 
 # Transmuxes FLV files to MP4
-function transmuxFLVToMP4 {
+transmuxFLVToMP4() {
+  local -r outputFolder="${1:-"."}"
+  cd "$outputFolder"
   for filename in *.+(flv|FLV); do
     # Extract filename without file extension
     local filenameBase=${filename%%.*}
     # Display info to user
     info "*** Transmuxing '$filename'... ***"
-    "$(command -v ffmpeg)" \
+    ffmpeg \
       -i "$filename" \
       -codec copy \
-      "$filenameBase.mp4" \
+      -loglevel error \
+      "$outputFolder/$filenameBase.mp4" \
     && info "*** '$filename' has been successfully transmuxed to '$filenameBase.mp4'. ***"
   done
   return 0
@@ -48,8 +70,17 @@ function transmuxFLVToMP4 {
 
 # Run functions
 trap exit 0 SIGINT # Press Ctrl+C to exit
-checkOS || { fatal "It seems you are not running macOS. Exiting..."; }
-checkFFmpegInstalled || { fatal "FFmpeg not found. Please install FFmpeg on your machine. See: https://trac.ffmpeg.org/wiki/CompilationGuide/MacOSX#ffmpegthroughHomebrew"; }
-transmuxFLVToMP4 || { fatal "Error transmuxing '$filename' to MP4"; }
+
+checkOS || error \
+"It seems you are not running macOS. Exiting..."
+
+checkInstalled ffmpeg || error \
+"FFmpeg not found. Please install, e.g. via Homebrew 'brew install ffmpeg'"
+
+checkFiles "$targetFolder" || error \
+"Could not find any FLV files."
+
+transmuxFLVToMP4 "$targetFolder" || error \
+"Error transmuxing '$filename' to MP4"
 
 exit 0
